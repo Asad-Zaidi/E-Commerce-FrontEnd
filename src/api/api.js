0b19b135e94d779f7f2e89e9afae52ccbc06371b
Syed Asad Jamil <syedasad1410@gmail.com>
2025-12-05 22:44:54 +0500

    refactor: Update API_BASE definition and log the base URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,10 @@
 
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000";
+// const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000";
 
 // const API_BASE = "https://edm-website-backend-production.up.railway.app";
+const API_BASE = "https://edm-website-backend.vercel.app";
 
 const api = axios.create({
     baseURL: API_BASE + "/api",
@@ -12,6 +13,8 @@ const api = axios.create({
         "Content-Type": "application/json",
     },
 })
+
+console.log("API Base URL:", API_BASE);
 
 export const setAuthToken = (token) => {
     if (token) api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
